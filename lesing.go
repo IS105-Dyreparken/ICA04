3182802a46d1c5b1d59a7805ed90a7e1a3ff0162
Konflux <marign16@student.uia.no>
2017-03-17 17:56:47 +0100

    Laget funksjon lineshift.go

--- lesing.go
+++ lesing.go
@@ -4,24 +4,23 @@ import (
 	"io"
 	"log"
 	"os"
-  "fmt"
-
+  	"fmt"
+	"strings"
 )
 
 
 
 func main() {
-  filepath := "/Users/sindrethompson/Desktop/GO/src/ICA04/is105-ica04/files/text1.txt"
-  filepath2 := "/Users/sindrethompson/Desktop/GO/src/ICA04/is105-ica04/files/text2.txt"
+  	filepath := "./files/text1.txt"
+  	filepath2 := "./files/text2.txt"
 	fileToByteslice(filepath)
-  fileToByteslice(filepath2)
+  	fileToByteslice(filepath2)
 }
 
 func fileToByteslice(filename string) []byte {
 
 
 	// Open file for reading
-
 	file, err := os.Open(filename)
 
 	if err != nil {
@@ -45,9 +44,46 @@ func fileToByteslice(filename string) []byte {
 		log.Fatal(err)
 	}
 
-  //fmt.Println(byteSlice)
-    fmt.Printf("%c", byteSlice)
-    //fmt.Printf("%s", byteSlice)
-    fmt.Printf("% X\n", byteSlice)
-  return (byteSlice)
+    	//fmt.Println(byteSlice)
+	//fmt.Printf("%c", byteSlice)
+    	//fmt.Printf("%s", byteSlice)
+    	//fmt.Printf("% X\n", byteSlice)
+		Lineshift(byteSlice)
+  	return (byteSlice)
+}
+
+
+func Lineshift(inputArray []byte) {
+
+	//Finner lengden på arrayet og justerer indeksene slik at man får siste og nest siste posisjon i byte-arrayet
+	lineFirst := len(inputArray)
+	lineLast := len(inputArray)
+	lineLast --
+	lineFirst --
+	lineLast --
+
+	//Henter ut bytes på siste og nest siste plass i arrayet og printer disse + hele arrayet, mest for debugging
+	arrFirst := inputArray[lineFirst]
+	arrLast := inputArray[lineLast]
+	//fmt.Printf("% X\n", arrFirst)
+	//fmt.Printf("% X\n", arrLast)
+	//fmt.Printf("% X\n", inputArray)
+	//fmt.Println() //Printer en tom linje for å lage litt plass
+
+	//Sammenligner de uthentede bytene fra arrayet med kode for CRLF og LF linjeskift
+	x := fmt.Sprintf("% X\n", arrFirst)
+	z := fmt.Sprintf("% X\n", arrLast)
+
+	var a = "A"
+	var b = "D"
+
+	//Fjerner først "whitespace" (newline, space osv), og sjekker deretter om strengen er stemmer overens med forskjellige formater for linjeskift
+	if strings.TrimSpace(x) == a && strings.TrimSpace(z) == b {
+		fmt.Println("Format = CRLF")
+	} else if strings.TrimSpace(x) == a && strings.TrimSpace(z) != b {
+		fmt.Println("Format = LF or other similar")
+	} else {
+		fmt.Println("Format = Unknown")
+	}
+
 }
