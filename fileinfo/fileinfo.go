6233c1f4a1f2c7809978e46c82e57e06c2a7b019
Konflux <marign16@student.uia.no>
2017-03-20 15:04:35 +0100

    oppdatert fileinfo

--- fileinfo/fileinfo.go
+++ fileinfo/fileinfo.go
@@ -2,47 +2,43 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
-var (
-	fileInfo os.FileInfo
-	err      error
-)
 
 func main() {
-	fmt.Print("Enter name of the file: ")
-	var input string
-	fmt.Scanln(&input)
+	fmt.Println("Enter the path to the file: ")
+	//var input string
+	var input = "./files/text1.txt"
+	//fmt.Scanln(&input)
 
-	fileInfo, err = os.Stat(input)
-	if err != nil {
-		log.Fatal(err)
-	}
+	file, _ := os.Stat(input)
 
-	fmt.Println("File name:", fileInfo.Name())      //fil navn
-	fmt.Println("Size in bytes:", fileInfo.Size())  //størrelse
-	fmt.Println("Is Directory: ", fileInfo.IsDir()) // Er det en mappe
-	{
-		if fileInfo.Mode().IsRegular() { // vanlig fil eller ikke
-			fmt.Println("Is a regular: true")
-		} else {
-			fmt.Println("Is a regular: false")
+	fmt.Println("File name:", file.Name())      // Returnerer filenavnet på valgt fil
+	fmt.Println("Size in bytes:", file.Size())  //størrelse
+	fmt.Println("Is Directory: ", file.IsDir()) // Er det en mappe?
+
+	regornot := file.Mode().IsRegular()
+		if regornot == true {
+			fmt.Println("This file is a regular file")
+		} else if regornot == false {
+			fmt.Println("This file is not a regular file")
 		}
-	}
+	/**
+	Hva skjer her??
 	if fileInfo.Mode()&os.ModeAppend == os.ModeAppend { // Deler filen data/info med andre filer
 		fmt.Println("Is Append: true")
 	} else {
 		fmt.Println("Is Append: false")
 	}
-	if fileInfo.Mode()&os.ModeDevice == os.ModeDevice { // En fil som lar software kommunisere med
-		fmt.Println("Is a device file: true") // "device driver" ved hjelp av standar
-	} else { // "input/output system calls"
+	**/
+
+	if file.Mode.()&os.ModeDevice == os.ModeDevice { // En fil som lar software kommunisere med "device driver" vha standard input/output systemkall"
+		fmt.Println("Is a device file: true")
+	} else {
 		fmt.Println("Is a device file: false")
 	}
 
-	fmt.Println("Permissions:", fileInfo.Mode())      // Hvilke rettigheter har man til filen
-	fmt.Println("Last modified:", fileInfo.ModTime()) // sist endring gjort på denne filen
-
+	fmt.Println("Has unix permissions: ", file.Mode())      // Hvilke rettigheter man har på filen
+	fmt.Println("Last modified: ", file.ModTime()) // Tiden hvor det sist ble gjort endring(er) i denne filen
 }
